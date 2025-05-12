1fec8790cca90df53082c2f2e2d46bf5bd5a5856
os-sr33 <olickalsreejith@gmail.com>
2025-05-12 13:07:09 +0530

    first commit

---
+++ secret.php
@@ -0,0 +1,23 @@
+<?php
+// Dummy AWS key
+$aws_secret = "AKIA123456789012345678";
+
+// Dummy GitHub token
+$github_token = "ghp_abcdefghijklmnopqrstuvwxyz1234567890";
+
+// Dummy Slack webhook
+$slack_webhook = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX";
+
+// Dummy private key
+$private_key = "
+-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEAwFAKEFAKEFAKEFAKEXXXXXXXXXXXXXX
+-----END RSA PRIVATE KEY-----
+";
+
+// Dummy password
+$db_password = "P@ssw0rd123!";
+
+// Dummy Google API Key
+$google_api_key = "AIzaSyA1234567890abcdefGHIJKLMNOPQRST";
+?>
